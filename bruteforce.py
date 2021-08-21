43d6949fab8c74f41e0ed2d9d1940d01efa8d4b5
Lionel TISSIER <contact@litiboost.fr>
2021-08-21 13:44:56 +0200

    modfi brute

--- bruteforce.py
+++ bruteforce.py
@@ -15,7 +15,7 @@ def open_csv_and_extract(file):
                     pass
                 else:
                     calcul_profit = round(float(row_csv[1]) * float(row_csv[2]) / 100, 2)
-                    if float(row_csv[1]) <= 0 or float(row_csv[2]) <= 0 :
+                    if float(row_csv[1]) <= 0 or float(row_csv[2]) <= 0:
                         pass
                     else:
                         actions[row_csv[0]] = dict({row_csv[0] + "_cost": float(row_csv[1]),
@@ -31,32 +31,38 @@ def sort_action_by(actions, benefice):
     sorted_by_dollar = sorted(benefice.items(), key=lambda x: x[0], reverse=True)
     return sorted_by_dollar
 
+
 def bruteforce(actions):
     action_name = [action for action, value in actions.items()]
     print(action_name)
-    liste = []
-    i=1
-    for action in action_name:
+    liste_of_possibility = []
+    possibility = []
+    index_action = 0
+    start_index = 1
+    for action in action_name[:1]:
         print(action)
-        tempory_list = action_name
-        possibility = action
-        tempory_list = tempory_list[i:]
+        possibility.append(action)
+        liste_of_possibility.append(possibility)
+        run = True
+        while run :
+            tempory_list = action_name[start_index:]
+            possibility.append(action_name[index_action])
+            for element in tempory_list:
+                possibility.append(element)
+                liste_of_possibility.append(possibility)
+            start_index +=1
+            possibility = []
+            if start_index > len(tempory_list):
+                run = False
 
-        liste.append(possibility)
-        for combinaison in tempory_list:
-            possibility += ", " + combinaison
-            liste.append(possibility)
-        i+=1
-    return liste
-
+    for element in liste_of_possibility :
+        print(element)
 
 if __name__ == "__main__":
     start_time = time.time()
     actions = open_csv_and_extract('csv/DEMO.csv')
     liste = bruteforce(actions)
     print(len(actions))
-    print(len(liste))
-    for element in liste :
-        print(element)
+    #print(len(liste))
 
     print("--- %s seconds ---" % (time.time() - start_time))
