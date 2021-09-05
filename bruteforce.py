74e1cabdbdcc72f3fadd9611919af007e0a46309
LiTiBe <contact@litiboost.fr>
2021-09-06 00:04:21 +0200

    maj brute

--- bruteforce.py
+++ bruteforce.py
@@ -48,7 +48,7 @@ def brutef(actions, longueur):
 if __name__ == "__main__":
     start_time = time.time()
     actions = open_csv_and_extract('csv/demo.csv')
-    longueur = 3
+    longueur = 5
     actions_name = [action[0] for action in actions]
     my_result = brutef(actions_name, longueur)
     print(len(my_result), "resultats de combinaison unique pour une longueur max de ", longueur, "elements")
