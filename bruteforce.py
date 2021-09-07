3eb09995d3d0868a8869b7050664a32cf6be5334
LiTiBe <contact@litiboost.fr>
2021-09-07 08:01:02 +0200

    maj brute

--- bruteforce.py
+++ bruteforce.py
@@ -79,7 +79,7 @@ def filter_by_size(my_result):
 if __name__ == "__main__":
     start_time = time.time()
     actions = open_csv_and_extract('csv/demo.csv')
-    longueur = 7
+    longueur = 5
     actions_name = [action[0] for action in actions]
     my_result = brutef(actions, longueur)
     print(len(my_result))
